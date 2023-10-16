7218c5be773476ccb3f55496727dccdf9f83b2ed
Martin Pruus <144675657+MartinP69@users.noreply.github.com>
2023-10-16 14:38:53 +0300

    Create Easy 2 ülesanne. (arvude summa array).js

---
+++ Easy 2 ülesanne. (arvude summa array).js
@@ -0,0 +1,19 @@
+array = [];
+let sum = 0;
+
+for(let i = 0; i<=2; i++) {
+    sisend = prompt('Kijruta 3 arvu mida liita kokku:')
+    array.push(sisend)
+} 
+
+for (let i = 0; i<=2; i++) {
+sum = sum + Number(array[i]);
+
+
+
+}
+
+console.log('Vastus:', sum)
+
+
+
