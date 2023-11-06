ff99ba7135eff3b64934802e0dfe91e4dbbd8894
Martin Pruus <144675657+MartinP69@users.noreply.github.com>
2023-11-06 15:04:26 +0200

    Create korrutusTabel.js

---
+++ korrutusTabel.js
@@ -0,0 +1,15 @@
+//Korrutus tabeli ülesanne.
+// 06.11.2023
+
+let a = 1;
+
+for (let i = 1; i <= 10; i++) {
+console.log('Korrutuse tabel: ' + a)
+a++
+
+    for (let j = 1; j <= 10; j++) {
+        tehe = i * j;
+        console.log(i + 'x' + j + '=' + tehe); 
+    }
+
+}
